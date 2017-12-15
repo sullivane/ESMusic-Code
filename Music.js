50db30e5d4c0e0c3929e14cdd225e23a9ce83251
sullivane <ewan.sullivan19@alumni.loyola.ca>
2017-12-15 09:22:32 -0500

    Add files via upload

---
+++ Music.js
@@ -0,0 +1,34 @@
+
+let C = 1;
+
+/* Sharps */
+let G = 2;
+let D = 3;
+let A = 4;
+let E = 5;
+let B = 6;
+let F# = 7;
+let C# = 8;
+
+/* Flats */
+let F = 9;
+let Bb = 10;
+let Eb = 11;
+let Ab = 12;
+let Db = 13;
+let Gb = 14;
+let Cb = 15;
+
+/* This randomizer will pick a random key signature. */
+console.log(Math.floor(Math.random() * 16));
+
+switch (C) {
+	case 1:
+		console.log('Key signature is C');
+			break;
+	case 2:
+		console.log(Key signature is G);
+			break;
+	
+	
+}
