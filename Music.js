187b2b4ea95c0da743bb3bc858e7b7dd9122b436
sullivane <ewan.sullivan19@alumni.loyola.ca>
2018-01-22 13:48:37 -0500

    Update Music.js

--- Music.js
+++ Music.js
@@ -6,7 +6,43 @@ let G = 2;
 let D = 3;
 let A = 4;
 let E = 5;
+let B = 6;/*
+let C = 1;
+
+let G = 2;
+let D = 3;
+let A = 4;
+let E = 5;
 let B = 6;
+let F# = 7;
+let C# = 8;
+
+let F = 9;
+let Bb = 10;
+let Eb = 11;
+let Ab = 12;
+let Db = 13;
+let Gb = 14;
+let Cb = 15;
+*/
+
+/* This randomizer will pick a random key signature. */
+
+console.log(Math.floor(Math.random() * 16));
+
+var KeySignature = 
+
+switch () {
+	case 1:
+		console.log('Key Signature is C major');
+			break;
+	case 2:
+		console.log('Key Signature is G Major');
+			break;
+	
+}
+
+
 let F# = 7;
 let C# = 8;
 
