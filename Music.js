4a927bf8e23ea377afe51c3651764ec7dc9d3d1a
sullivane <ewan.sullivan19@alumni.loyola.ca>
2018-02-22 10:24:32 -0500

    Update Music.js

--- Music.js
+++ Music.js
@@ -1,104 +1,13 @@
+/* Music Key/Chord Generator */
 
-let C = 1;
-
-/* Sharps */
-let G = 2;
-let D = 3;
-let A = 4;
-let E = 5;
-let B = 6;/*
-let C = 1;
-
-let G = 2;
-let D = 3;
-let A = 4;/*
-let C = 1;
-
-let G = 2;
-let D = 3;
-let A = 4;
-let E = 5;
-let B = 6;
-let F# = 7;
-let C# = 8;
-
-let F = 9;
-let Bb = 10;
-let Eb = 11;
-let Ab = 12;
-let Db = 13;
-let Gb = 14;
-let Cb = 15;
-*/
-
-/* This randomizer will pick a random key signature. */
-
-var KeySignature = Math.floor(Math.random() * 16);
-
-switch (KeySignature) {
-	case 1:
-		console.log('Key Signature is C major');
-			break;
-	case 2:
-		console.log('Key Signature is G Major');
-			break;
+function Music() {
 	
+	var Keys = Math.floor((Math.random() * 15) + 1);
+    document.getElementById('').value;
+	
+	switch (Keys) {
+		case 'C':
+			 var key = document.getElementsByClassName("C");
+	}
 }
 
-
-let E = 5;
-let B = 6;
-let F# = 7;
-let C# = 8;
-
-let F = 9;
-let Bb = 10;
-let Eb = 11;
-let Ab = 12;
-let Db = 13;
-let Gb = 14;
-let Cb = 15;
-*/
-
-/* This randomizer will pick a random key signature. */
-
-console.log(Math.floor(Math.random() * 16));
-
-var KeySignature = 
-
-switch () {
-	case 1:
-		console.log('Key Signature is C major');
-			break;
-	case 2:
-		console.log('Key Signature is G Major');
-			break;
-	
-}
-
-
-let F# = 7;
-let C# = 8;
-
-/* Flats */
-let F = 9;
-let Bb = 10;
-let Eb = 11;
-let Ab = 12;
-let Db = 13;
-let Gb = 14;
-let Cb = 15;
-
-/* This randomizer will pick a random key signature. */
-console.log(Math.floor(Math.random() * 16));
-
-switch (C) {
-	case 1:
-		console.log('Key signature is C');
-			break;
-	case 2:
-		console.log(Key signature is G);
-			break;
-	
-	
-}
