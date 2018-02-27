5ee650b92160a921911267578546e67a9cbdee96
sullivane <ewan.sullivan19@alumni.loyola.ca>
2018-02-27 09:18:55 -0500

    Update Music.js

--- Music.js
+++ Music.js
@@ -1,13 +1,24 @@
 /* Music Key/Chord Generator */
 
-function Music() {
+function MusicCode() {
+	
+	var Keys = document.getElementById('Music').value;
 	
 	var Keys = Math.floor((Math.random() * 15) + 1);
-    document.getElementById('').value;
 	
 	switch (Keys) {
-		case 'C':
-			 var key = document.getElementsByClassName("C");
+		case 1:
+			document.getElementById("C").innerHTML = "Key of C";
+			 
+				break;
+		case 2:
+			document.getElementById("G").innerHTML = "Key of G";
+			
+				break;
+		case 3:
+			document.getElementById("D").innerHTML = "Key of D";
+			
+				break;
 	}
 }
 
